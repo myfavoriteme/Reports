32419c735573ae83e6295d4a5b05eb308cae73fe
myfavoriteme <vlad.kakorin03@gmail.com>
2022-09-05 17:48:46 +0300

    feat: add phone test

--- script.js
+++ script.js
@@ -10,3 +10,38 @@
     
 //     console.log(a);
 // }));
+
+// console.log(navigator.userAgent);
+function IsPC() {
+             var userAgentInfo = navigator.userAgent;
+             var Agents = ["Android", "iPhone",
+                         "SymbianOS", "Windows Phone",
+                         "iPad", "iPod"];
+             var flag = true;
+             for (var v = 0; v < Agents.length; v++) {
+                 if (userAgentInfo.indexOf(Agents[v]) > 0) {
+                     flag = false;
+                 break;
+                }
+            }
+            return flag;
+}
+
+let phone = '';
+
+var u = navigator.userAgent;
+    if (u.indexOf('Android') > -1 || u.indexOf('Linux') > -1) {
+        phone = 'Android';
+    } 
+        else if (u.indexOf('iPhone') > -1) {
+            phone = 'iPhone';
+    } 
+        else if (u.indexOf('Windows Phone') > -1) {
+            phone = 'winphone';
+}
+
+console.log(IsPC()) 
+console.log(phone)
+
+document.querySelector('.test1').innerHTML = IsPC();
+document.querySelector('.test2').innerHTML = phone;
