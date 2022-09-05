71ba74721f6db4ce6aa6bdbf378c0598b40d6264
myfavoriteme <vlad.kakorin03@gmail.com>
2022-09-05 17:00:08 +0300

    fix: trying to fix this mess

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-console.log('hello');
+// console.log('hello');
 
-let items = document.querySelectorAll('.item');
+// let items = document.querySelectorAll('.item');
 
-// Show description
-items.forEach(b=>b.addEventListener('click', function ShowDescription() {
-    let a = '.' + this.getAttribute('num');
-    document.querySelector(a).classList.toggle('show-description');
-    // this.getAttribute('')
+// // Show description
+// items.forEach(b=>b.addEventListener('click', function ShowDescription() {
+//     let a = '.' + this.getAttribute('num');
+//     document.querySelector(a).classList.toggle('show-description');
+//     // this.getAttribute('')
     
-    console.log(a);
-}));
+//     console.log(a);
+// }));
